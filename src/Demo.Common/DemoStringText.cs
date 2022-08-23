0ad317a9dea681eed21b3a948a03e1931db1cd48
Peter Chamley <peter@chamley.name>
2022-08-23 09:53:31 +0100

    Update1

--- src/Demo.Common/DemoStringText.cs
+++ src/Demo.Common/DemoStringText.cs
@@ -4,5 +4,8 @@
     {
         public static string CombineStrings(string String1, string Srting2)
         { return $"{String1}{Srting2}";}
+
+        public static string CombineStrings(string String1, string Srting2, string Srting3)
+        { return $"{String1} {Srting2} {Srting3}"; }
     }
 }
